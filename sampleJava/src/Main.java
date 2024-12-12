72a16318a36538d1b2426a00dd176138a3d1705f
calmero <mdl.jals107@gmail.com>
2024-12-12 11:41:25 +0700

    interview note - p11

--- sampleJava/src/Main.java
+++ sampleJava/src/Main.java
@@ -1,5 +1,10 @@
+import spi.LoggerService;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		LoggerService service = LoggerService.getService();
+
+		service.info("Hello SPI");
+		service.debug("Hello SPI");
 	}
 }
